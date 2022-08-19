8767675d4d40344f17e654fd72991490f4ece0a3
Guilherme Raposo <gui.raposo36@gmail.com>
2022-08-18 23:17:30 -0300

    feat: created password and passwordRepeat functions to disable button

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,9 +1,74 @@
-import SignUpPage from './SignUpPage.vue'
-import { render, screen } from '@testing-library/vue'
-import "testing-library/jest-dom"
+import SignUpPage from "./SignUpPage.vue";
+import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event"
 
-it('has Sign Up Header', () => {
-    render(SignUpPage)
-    const header = screen.queryByRole('heading', { name: 'Sign Up' })
-    expect(header).toBeInTheDocument()
-})
+describe("Sign Up Page", () => {
+    describe("Layout", () => {
+        it("has Sign Up Header", () => {
+            render(SignUpPage);
+            const header = screen.queryByRole("heading", { name: "Sign Up" });
+            expect(header).not.toBeNull();
+        });
+
+        it('has username input', () => {
+            render(SignUpPage);
+            const input = screen.queryByLabelText('Username')
+            expect(input).not.toBeNull()
+        })
+
+        it('has email input', () => {
+            render(SignUpPage);
+            const input = screen.queryByLabelText('E-mail')
+            expect(input).not.toBeNull()
+        })
+
+        it('has password input', () => {
+            render(SignUpPage);
+            const input = screen.queryByLabelText('Password')
+            expect(input).not.toBeNull()
+        })
+
+        it('has password type for password input', () => {
+            render(SignUpPage);
+            const input = screen.queryByLabelText('Password')
+            expect(input.type).toBe("password")
+        })
+
+        it('has password repeat input', () => {
+            render(SignUpPage);
+            const input = screen.queryByLabelText('Password Repeat')
+            expect(input).not.toBeNull()
+        })
+
+        it('has password type for password repeat input', () => {
+            render(SignUpPage);
+            const input = screen.queryByLabelText('Password Repeat')
+            expect(input.type).toBe("password")
+        })
+
+        it("has Sign Up Button", () => {
+            render(SignUpPage);
+            const button = screen.queryByRole("button", { name: "Sign Up" });
+            expect(button).not.toBeNull();
+        });
+
+        it("disables the button initially", () => {
+            render(SignUpPage);
+            const button = screen.queryByRole("button", { name: "Sign Up" });
+            expect(button).toBeDisabled;
+        });
+    });
+
+    describe('Interactions', () => {
+        it('enables the button when the password and password repeat fields have same values', async () => {
+            render(SignUpPage);
+            const passwordInput = screen.queryByLabelText('Password')
+            const passwordRepeatInput = screen.queryByLabelText('Password Repeat')
+            await userEvent.type(passwordInput, "P4ssword")
+            await userEvent.type(passwordRepeatInput, "P4ssword")
+            const button = screen.queryByRole("button", { name: "Sign Up" });
+            expect(button).toBeEnabled;
+        })
+    })
+
+});
--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,7 +1,36 @@
-<script lang="ts" setup></script>
+<script>
+export default {
+  name: "SignUpPage",
+  data() {
+    return {
+      password: "",
+      passwordRepeat: "",
+    };
+  },
+
+  computed: {
+    isDisabled() {
+      return this.password && this.passwordRepeat
+        ? this.password !== this.passwordRepeat
+        : true;
+    },
+  },
+};
+</script>
+
 <template>
-  <div>
-    <h1>Sign Up</h1>
-  </div>
+  <h1>Sign Up</h1>
+  <label for="username">Username</label>
+  <input id="username" type="text" />
+
+  <label for="email">E-mail</label>
+  <input id="email" type="email" />
+
+  <label for="password">Password</label>
+  <input id="password" type="password" v-model="password" />
+
+  <label for="password-repeat">Password Repeat</label>
+  <input id="password-repeat" type="password" v-model="passwordRepeat" />
+
+  <button :disabled="isDisabled">Sign Up</button>
 </template>
-<style lang="scss" scoped></style>
