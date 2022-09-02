e2e175b7d91a4a7693560090cc9b1df345020e22
Guilherme Raposo <gui.raposo36@gmail.com>
2022-09-01 23:08:33 -0300

    feat: implements test with backend conection

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,11 +1,25 @@
 <script>
+import axios from "axios";
+
 export default {
   name: "SignUpPage",
   data() {
     return {
+      username: "",
+      email: "",
       password: "",
       passwordRepeat: "",
     };
+  },
+
+  methods: {
+    submit() {
+      axios.post("/api/1.0/users", {
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      });
+    },
   },
 
   computed: {
@@ -19,18 +33,20 @@ export default {
 </script>
 
 <template>
-  <h1>Sign Up</h1>
-  <label for="username">Username</label>
-  <input id="username" type="text" />
+  <form>
+    <h1>Sign Up</h1>
+    <label for="username">Username</label>
+    <input id="username" type="text" v-model="username" />
 
-  <label for="email">E-mail</label>
-  <input id="email" type="email" />
+    <label for="email">E-mail</label>
+    <input id="email" type="email" v-model="email" />
 
-  <label for="password">Password</label>
-  <input id="password" type="password" v-model="password" />
+    <label for="password">Password</label>
+    <input id="password" type="password" v-model="password" />
 
-  <label for="password-repeat">Password Repeat</label>
-  <input id="password-repeat" type="password" v-model="passwordRepeat" />
+    <label for="password-repeat">Password Repeat</label>
+    <input id="password-repeat" type="password" v-model="passwordRepeat" />
 
-  <button :disabled="isDisabled">Sign Up</button>
+    <button :disabled="isDisabled" @click.prevent="submit">Sign Up</button>
+  </form>
 </template>
