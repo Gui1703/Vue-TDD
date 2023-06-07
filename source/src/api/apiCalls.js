df96ca8f131f383ea3597df07ef44cbaf9c6f351
Guilherme Raposo <gui.raposo36@gmail.com>
2023-06-07 00:23:59 -0300

    feat: account activation api request

--- source/src/api/apiCalls.js
+++ source/src/api/apiCalls.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { i18n } from "../locales/i18n";
 
-export const signUp = (body) => {
-  return axios.post("/api/1.0/users", body, {
+export const signUp = async (body) => {
+  return await axios.post("/api/1.0/users", body, {
     headers: { "Accept-Language": i18n.global.locale },
   });
+};
+
+export const activate = async (token) => {
+  return await axios.post(`/api/1.0/token/${token}`);
 };
--- source/src/pages/AccountActivationPage.vue
+++ source/src/pages/AccountActivationPage.vue
@@ -1,10 +1,35 @@
 <script>
-export default {};
+import { activate } from "../api/apiCalls";
+export default {
+  data() {
+    return {
+      success: false,
+      fail: false,
+      apiProgress: false,
+    };
+  },
+  async mounted() {
+    try {
+      this.success = true;
+      this.apiProgress = true;
+      await activate(this.$route.params.token);
+    } catch (e) {
+      this.fail = true;
+    } finally {
+      this.apiProgress = false;
+    }
+  },
+};
 </script>
 
 <template>
   <div data-testid="activation-page">
-    <h1>Account Activation</h1>
+    <div class="alert alert-success mt-3" v-if="success">
+      Account is activated
+    </div>
+    <div class="alert alert-danger mt-3" v-if="fail">Activation failure</div>
+
+    <span v-if="apiProgress" class="spinner-border" role="status" />
   </div>
 </template>
 
