dcc2581b226c2c2028eed00baf0edd983448ea9e
Guilherme Raposo <gui.raposo36@gmail.com>
2023-06-08 13:55:57 -0300

    feat: default image

--- source/src/components/UserListItem.vue
+++ source/src/components/UserListItem.vue
@@ -1,11 +1,21 @@
 <script>
-export default { props: { user: { username: String, image: String } } };
+import imageDefault from "../assets/profile.png";
+export default {
+  props: { user: { username: String, image: String } },
+  computed: {
+    getImage() {
+      return typeof this.user.image === "string"
+        ? this.user.image
+        : imageDefault;
+    },
+  },
+};
 </script>
 
 <template>
   <img
     class="rounded-circle shadow-sm"
-    src="../assets/profile.png"
+    :src="getImage"
     width="30"
     alt="profile"
   />
