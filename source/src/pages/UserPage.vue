721b876e0873c5bdcb362bd5ac64877f071e01a2
Guilherme Raposo <gui.raposo36@gmail.com>
2023-06-08 16:40:01 -0300

    feat: UserPage Progress Indicator

--- source/src/pages/UserPage.vue
+++ source/src/pages/UserPage.vue
@@ -1,23 +1,36 @@
 <script>
 import { getUserById } from "../api/apiCalls";
 import ProfileCard from "../components/ProfileCard.vue";
+import Spinner from "../components/Spinner.vue";
 
 export default {
   name: "User Page",
-  components: { ProfileCard },
+  components: { Spinner, ProfileCard },
   data() {
-    return { user: {} };
+    return { user: {}, pendingApiCall: true, failResponse: undefined };
   },
   async mounted() {
-    const { data } = await getUserById(this.$route.params.id);
-    this.user = data;
+    try {
+      const { data } = await getUserById(this.$route.params.id);
+      this.user = data;
+    } catch (error) {
+      this.failResponse = error.response.data.message;
+    } finally {
+      this.pendingApiCall = false;
+    }
   },
 };
 </script>
 
 <template>
   <div data-testid="user-page">
-    <ProfileCard :user="user" />
+    <ProfileCard :user="user" v-if="!pendingApiCall && !failResponse" />
+    <div class="alert alert-secondary text-center" v-if="pendingApiCall">
+      <Spinner size="normal" />
+    </div>
+    <div class="alert alert-danger text-center" v-if="failResponse">
+      {{ failResponse }}
+    </div>
   </div>
 </template>
 
